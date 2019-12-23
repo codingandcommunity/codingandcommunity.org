1c17f42e11d1887b42ea7f8dfdd4fc988ecc367e
eagraf <graf.ethan@gmail.com>
2019-12-23 17:06:46 -0500

    Links to pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
 import styled from "@emotion/styled";
-import colors from "styles/colors";
 import dimensions from "styles/dimensions";
 import Logo from "components/_ui/Logo";
 
@@ -32,11 +31,10 @@ const HeaderContent = styled("div")`
 
 const HeaderLinks = styled("div")`
     display: grid;
-    grid-template-columns: repeat(2, auto);
-    grid-gap: 7em;
+    grid-template-columns: repeat(4, auto);
+    grid-gap: 3em;
     justify-content: flex-end;
     width: 100%;
-    max-width: 200px;
 
     @media(max-width: ${dimensions.maxwidthTablet}px) {
         grid-gap: 5.5em;
@@ -50,7 +48,7 @@ const HeaderLinks = styled("div")`
         color: white;
         text-decoration: none;
         border-bottom: 3px solid transparent;
-        font-weight: 600;
+        font-weight: 500;
         font-size: 0.95em;
         height: 100%;
         padding-bottom: 0.25em;
@@ -95,8 +93,18 @@ const Header = () => (
             <HeaderLinks>
                 <Link
                     activeClassName="Link--is-active"
+                    to="/programs">
+                    Programs
+                </Link>
+                <Link
+                    activeClassName="Link--is-active"
                     to="/work">
-                    Work
+                    Work With Us
+                </Link>
+                <Link
+                    activeClassName="Link--is-active"
+                    to="/team">
+                    Team
                 </Link>
                 <Link
                     activeClassName="Link--is-active"
