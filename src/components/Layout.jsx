ad1a696837c122fe35029b292c9db7812b7856f8
pathfold <pathfolder@gmail.com>
2021-02-17 18:48:39 -0500

    added icons

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -68,6 +68,9 @@ const Layout = ({ children }) => (
                     style {
                         url
                     }
+                    replit {
+                        url
+                    }
                 }
             }
         `}
